466f066d85820508522a0f96b9d62d69f112ec78
Sunzidabeg <50387582+Sunzidabeg@users.noreply.github.com>
2019-08-11 19:59:38 -0500

    Add files via upload

---
+++ src/main/java/com/codingdojo/thecode/controllers/CodeController.java
@@ -0,0 +1,29 @@
+package com.codingdojo.thecode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class CodeController {
+    @RequestMapping("/")
+    public String index(@ModelAttribute("error") String error){
+        return "/WEB-INF/views/index.jsp";
+    }
+    @RequestMapping("/code")
+    public String code(){
+        return "x.html";
+    }
+    @PostMapping("/code")
+    public String code(@RequestParam("code") String code, RedirectAttributes redirectAttributes){
+        if(code.equals("bushido")){
+            return "redirect:/code";
+        }else{
+            redirectAttributes.addFlashAttribute("error", "Train harder!");
+            return"redirect:/";
+        }
+    }
+}
